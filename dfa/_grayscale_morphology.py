c92b6744216c0ab1f36f99d5a606467ec60e99be
julien.pontabry <julien.pontabry@helmholtz-muenchen.de>
2017-05-11 14:19:22 +0200

    Add fast adjunct operators.

--- dfa/_grayscale_morphology.py
+++ dfa/_grayscale_morphology.py
@@ -110,3 +110,91 @@ def varying_closing(image, structuring_elements):
     """
     return varying_erosion(varying_dilation(image, structuring_elements),
                            structuring_elements)
+
+
+def adjunct_varying_filtering_2d(image, structuring_elements, function_map,
+                                 function_reduce, initialization):
+    """
+    Compute the grayscale adjunct filtering with a (possibly) varying
+    structuring element.
+
+    This is particularly important when using varying structuring elements, as
+    the standard operators do not take into account the varying elements.
+
+    :param image: Image to filter.
+    :type image: numpy.ndarray
+
+    :param structuring_elements: Structuring elements possibly varying.
+    :type structuring_elements: numpy.ndarray
+
+    :param function_map: Function of the filter domain used when mapping.
+    :type function_map: function
+
+    :param function_reduce: Function of the filter domain used when reducing.
+    :type function_reduce: function
+
+    :param initialization: Initialization element.
+    :type initialization: float or int
+
+    :return: The filtered image with the adjunct operator.
+    :rtype: numpy.ndarray
+    """
+    dilation = np.zeros(image.shape)
+    dilation[:] = initialization
+
+    ki = structuring_elements.shape[2] // 2
+    kj = structuring_elements.shape[3] // 2
+
+    oj, oi = np.meshgrid(range(-kj, kj + 1), range(-ki, ki + 1))
+
+    for i in range(ki, image.shape[0] - ki):
+        for j in range(kj, image.shape[1] - kj):
+            dilation[i + oi, j + oj] = function_reduce(
+                dilation[i + oi, j + oj],
+                function_map(image[i, j], structuring_elements[i, j, :]))
+
+    dilation[dilation == initialization] = 0
+
+    return dilation
+
+
+def adjunct_varying_dilation(image, structuring_elements):
+    """
+    Compute the grayscale adjunct dilation to the erosion with a (possibly)
+    varying structuring element.
+
+    This is particularly important when using varying structuring elements, as
+    the standard operators do not take into account the varying elements.
+
+    :param image: Image to filter.
+    :type image: numpy.ndarray
+
+    :param structuring_elements: Structuring elements possibly varying.
+    :type structuring_elements: numpy.ndarray
+
+    :return: The dilated image with the adjunct of the erosion.
+    :rtype: numpy.ndarray
+    """
+    return adjunct_varying_filtering_2d(image, structuring_elements,
+                                        np.add, np.maximum, -np.inf)
+
+
+def adjunct_varying_erosion(image, structuring_elements):
+    """
+    Compute the grayscale adjunct erosion to the dilation with a (possibly)
+    varying structuring element.
+
+    This is particularly important when using varying structuring elements, as
+    the standard operators do not take into account the varying elements.
+
+    :param image: Image to filter.
+    :type image: numpy.ndarray
+
+    :param structuring_elements: Structuring elements possibly varying.
+    :type structuring_elements: numpy.ndarray
+
+    :return: The eroded image with the adjunct of the dilation.
+    :rtype: numpy.ndarray
+    """
+    return adjunct_varying_filtering_2d(image, structuring_elements,
+                                        np.subtract, np.minimum, np.inf)
