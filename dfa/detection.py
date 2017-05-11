702db2b863a5235e3cd5af57279ba2bd8437cd45
julien.pontabry <julien.pontabry@helmholtz-muenchen.de>
2017-05-11 15:30:17 +0200

    Implement the fibers reconstruction pipeline.

--- dfa/detection.py
+++ dfa/detection.py
@@ -7,12 +7,14 @@ Note that only quite straight fibers are detected for now.
 """
 import numpy as np
 
-import _scale_space_hessian as _sha
+from dfa import _scale_space_hessian as _sha
+from dfa import _structuring_segments as _ss
+from dfa import _grayscale_morphology as _gm
 
 
 def vesselness_filter(image, scales, alpha, beta):
     """
-    Enhance fiber image using a multiscale vesselness filter.
+    Enhance fiber image using a multi-scale vesselness filter.
 
     This vesselness is based on the Frangi filter (Frangi et al. Multiscale
     vessel enhancement filtering (1998). In: Medical ImageComputing and
@@ -55,8 +57,46 @@ def vesselness_filter(image, scales, alpha, beta):
                        np.tile(np.arange(sj).reshape(1, 1, sj), (2, si, 1))])
 
 
-def reconstruct_fibers(vesselness, principal_directions, length):
-    pass  # TODO: implement fibers reconstruction
+def reconstruct_fibers(vesselness, directions, length, size):
+    """
+    Reconstruct fibers disconnections from vesselness map and directions of
+    less intensity variations.
+
+    The reconstruction is based on morphological closing using variant
+    structuring segment. This algorithm is based on the following publication:
+    Tankyevych et al. Curvilinear morpho-Hessian filter (2008). In: 5th IEEE
+    International Symposium on Biomedical Imaging: From Nano to Macro, pp.
+    1011-1014
+
+    :param vesselness: Input vesselness map.
+    :type vesselness: numpy.ndarray
+
+    :param directions: Vector field of less intensity variations of input image.
+    :type directions: numpy.ndarray
+
+    :param length: Length of the structuring segment.
+    :type length: strictly positive int
+
+    :param size: Thickness of the structuring segment.
+    :type size: strictly positive int
+
+    :return: The reconstructed vesselness map.
+    :rtype: numpy.ndarray
+    """
+    # We need to flip the vectors components (x corresponds to j, and y to i)
+    directions = np.flip(directions, axis=0)
+
+    # Regularize the vector field with a simple morphological dilation
+    segments = _ss.structuring_segments(directions, size, length, 0)
+    regularized_directions = np.zeros(directions.shape)
+    regularized_directions[0] = _gm.adjunct_varying_dilation(
+        directions[0], segments)
+    regularized_directions[1] = _gm.adjunct_varying_dilation(
+        directions[1], segments)
+    segments = _ss.structuring_segments(regularized_directions, size, length, 0)
+
+    # Reconstruct by morphological closing
+    return _gm.adjunct_varying_closing(vesselness, segments)
 
 
 def estimate_medial_axis(reconstruction):
@@ -74,6 +114,12 @@ if __name__ == '__main__':
     parser.add_argument('--scales', type=float, nargs=3, default=[3, 5, 5],
                         help='Scales to use in pixels (minimum, maximum, '
                              'number of scales).')
+    parser.add_argument('--no-flat', action='store_true',
+                        help='Use non-flat structuring elements for fiber '
+                             'reconstruction (by default use flat structuring '
+                             'elements).')
+    parser.add_argument('--reconstruction-extent', type=float, default=20,
+                        help='Reconstruction extent in pixels (default is 20).')
     args = parser.parse_args()
 
     input_image = io.imread(args.input)
@@ -83,3 +129,7 @@ if __name__ == '__main__':
         np.linspace(args.scales[0], args.scales[1],
                     int(args.scales[2])).tolist(),
         alpha=args.alpha, beta=args.beta)
+
+    reconstructed_vesselness = reconstruct_fibers(
+        vesselness, directions, args.reconstruction_extent,
+        (args.scales[0]+args.scales[1])/2)
