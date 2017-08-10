ab0f4243de747c13ebc943db3f87421ec3670545
julien.pontabry <julien.pontabry@helmholtz-muenchen.de>
2017-08-10 13:42:41 +0200

    Add version number generation code as comment in package init.

--- dfa/__init__.py
+++ dfa/__init__.py
@@ -4,4 +4,11 @@ DNA fibers analysis package.
 __all__ = ['modeling', 'detection', 'extraction', 'analysis', 'simulation']
 
 # gold number (continuous fraction approximation, current iteration is 5)
+# def dfa_version(n):
+#     def _approx_gold_rec(e, n):
+#         if n == 0:
+#             return 1 + e
+#         else:
+#             return _approx_gold_rec(1/(1 + e), n - 1)
+#     return str(_approx_gold_rec(1, n))
 __version__ = '1.6153846153846154'
